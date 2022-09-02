347b329f756892f3cc9b7d329306e96793b69afa
Marcos Bautista P <macondo777@hotmail.com>
2022-09-02 12:49:54 -0500

    Versiom 1.5.1 creando controladores y servicios de las entidades

--- src/main/java/com/TheDummiesDev/controllers/JacController.java
+++ src/main/java/com/TheDummiesDev/controllers/JacController.java
@@ -5,17 +5,25 @@ import com.TheDummiesDev.servicios.JacService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 
 //controlador de la entidad junta de acción comunal (Jac)
 @RestController
 public class JacController {
     JacService servicio;
     public JacController(){
+
         this.servicio = new JacService();
     }
 
-    @GetMapping("/Jac")
-    public Jac Jac1() {
+    @GetMapping("/listaJac")
+    public List<Jac> listaJac() {
+        return this.servicio.getListaJac();
+    }
+
+    @GetMapping("/Jac1")
+    public Jac Jac1(){
         return this.servicio.getJac1();
     }
 }
--- src/main/java/com/TheDummiesDev/servicios/JacService.java
+++ src/main/java/com/TheDummiesDev/servicios/JacService.java
@@ -2,17 +2,30 @@ package com.TheDummiesDev.servicios;
 
 import com.TheDummiesDev.entities.Jac;
 
+import java.util.ArrayList;
+import java.util.List;
+
 //Clase del servicio junta de acción comunal
 public class JacService {
     Jac jac1;
+    Jac jac2;
+    List<Jac> listaJac;
 
     //constructor del servicio de la junta de acción comunal
     public JacService(){
         this.jac1 = new Jac("La Prosperidad","cl 3 # 3-27","727 6420","1134-5");
-
+        this.jac2 = new Jac("La Paz","cl 34 # 12-45","7274523","12234-4");
+        this.listaJac = new ArrayList<>();
+        listaJac.add(jac1);
+        listaJac.add(jac2);
 
     }
-    //metedo del servicio de la junta de accion comunal
+
+    //Metodo getter de la lista Jac (junta de accion comunal).
+    public List<Jac> getListaJac() {
+        return this.listaJac;
+    }
+    //Getter para consultar jac1
     public Jac getJac1(){
         return this.jac1;
     }
